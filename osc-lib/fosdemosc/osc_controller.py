1d58f402a33b3006f791e9c030146462255876d2
Albert Stefanov <albert@kmail.bg>
2025-01-06 08:48:46 +0200

    osc-lib: query multipliers and mutes

--- osc-lib/fosdemosc/osc_controller.py
+++ osc-lib/fosdemosc/osc_controller.py
@@ -23,20 +23,24 @@ class OSCController:
         message = OscMessageBuilder(f"/{specifier}/{num}/config/name")
         self.client.send(message.build())
         response = self.client.receive_message()
-        return response.params[0]
+        return str(response.params[0])
 
-    def __get_channel_name(self, num: int) -> str:
-        return self.__get_chbus_name('ch', num)
+    def __get_chbus_multiplier(self, specifier: str, num: int) -> float:
+        message = OscMessageBuilder(f"/{specifier}/{num}/multiplier")
+        self.client.send(message.build())
+        response = self.client.receive_message()
+        return float(response.params[0])
 
+    def __set_chbus_multiplier(elf, specifier: str, num: int, multiplier: float):
+        message = OscMessageBuilder(f"/{specifier}/{num}/multiplier")
+        message.add_arg(float(multiplier))
+        self.client.send(message.build())
 
     def __get_inputs(self) -> List[str]:
-        return [self.__get_channel_name(x) for x in range(0, NUM_CHANNELS)]
-
-    def __get_bus_name(self, num: int) -> str:
-        return self.__get_chbus_name('bus', num)
+        return [self.__get_chbus_name('ch', x) for x in range(0, NUM_CHANNELS)]
 
     def __get_outputs(self) -> List[str]:
-        return [self.__get_bus_name(x) for x in range(0, NUM_BUSES)]
+        return [self.__get_chbus_name('bus', x) for x in range(0, NUM_BUSES)]
 
     @property
     def device(self) -> str:
@@ -57,13 +61,43 @@ class OSCController:
         self.client.send(message.build())
 
         response = self.client.receive_message()
-        level = response.params[0]
+        return Level(response.params[0])
 
-        return level
+    def get_bus_multiplier(self, bus: Bus) -> Level:
+        return __get_chbus_multiplier('bus', bus)
+
+    def get_channel_multiplier(self, channel: Channel) -> Level:
+        return __get_chbus_multiplier('ch', channel)
+
+    def get_raw_gain(self, channel: Channel, bus: Bus) -> Level:
+        message = OscMessageBuilder(f"/ch/{channel}/mix/{bus}/raw")
+        self.client.send(message.build())
+
+        response = self.client.receive_message()
+        return Level(response.params[0])
 
     def set_gain(self, channel: Channel, bus: Bus, level: Level) -> None:
         message = OscMessageBuilder(f"/ch/{channel}/mix/{bus}/level")
         message.add_arg(Level(level))
         self.client.send(message.build())
 
-        assert abs(self.get_gain(channel, bus) - level) < 0.01  # up to 1% error
+    def get_muted(self, channel: Channel, bus: Bus) -> bool:
+        message = OscMessageBuilder(f"/ch/{channel}/mix/{bus}/muted")
+        self.client.send(message.build())
+
+        response = self.client.receive_message()
+        return bool(response.params[0])
+
+    def set_muted(self, channel: Channel, bus: Bus, muted: bool) -> None:
+        message = OscMessageBuilder(f"/ch/{channel}/mix/{bus}/muted")
+        message.add_arg(bool(muted))
+        self.client.send(message.build())
+
+    def get_state(self):
+        message = OscMessageBuilder(f"/state")
+        self.client.send(message.build())
+
+        response = self.client.receive_bundle()
+
+        return {x.address: x.params[0] for x in response}
+
